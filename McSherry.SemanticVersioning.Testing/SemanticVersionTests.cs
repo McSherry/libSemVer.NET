1c37854bdf2151239e58d5f14e5f1ec394dc717c
Liam McSherry <mcsherry.liam@gmail.com>
2017-04-06 23:11:05 +0100

    Convert test assembly to .NET Core.
    MSTest works with .NET Core, but not .NET Standard. However, they should
    be identical, so it doesn't matter too much. Tests for Standard and Core
    is the ideal situation, but we're not in that situation and there's lots
    of converting to be done if we're to use xUnit.
    
    Tests for [BinaryFormatter] are omitted because it isn't in .NET Core.
    
    Other minor (i.e. no behaviour-changing) adaptions are also made.

--- McSherry.SemanticVersioning.Testing/SemanticVersionTests.cs
+++ McSherry.SemanticVersioning.Testing/SemanticVersionTests.cs
@@ -21,7 +21,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+//using System.Runtime.Serialization.Formatters.Binary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using static System.Linq.Enumerable;
@@ -492,38 +492,38 @@ namespace McSherry.SemanticVersioning
         /// as expected.
         /// </para>
         /// </summary>
-        [TestMethod, TestCategory(Category)]
-        public void BinarySerialisation()
-        {
-            var serialiser = new BinaryFormatter();
-            var stream = new MemoryStream();
+        //[TestMethod, TestCategory(Category)]
+        //public void BinarySerialisation()
+        //{
+        //    var serialiser = new BinaryFormatter();
+        //    var stream = new MemoryStream();
 
-            // We want to make sure everything is serialised properly,
-            // so we're going to need a version with all the bells and
-            // whistles.
-            var sv = SemanticVersion.Parse("1.5.6-rc.1+2015.10.18");
+        //    // We want to make sure everything is serialised properly,
+        //    // so we're going to need a version with all the bells and
+        //    // whistles.
+        //    var sv = SemanticVersion.Parse("1.5.6-rc.1+2015.10.18");
 
-            // Serialise the version, then seek the stream back to the
-            // start so we can deserialise it.
-            serialiser.Serialize(stream, sv);
-            stream.Seek(0, SeekOrigin.Begin);
+        //    // Serialise the version, then seek the stream back to the
+        //    // start so we can deserialise it.
+        //    serialiser.Serialize(stream, sv);
+        //    stream.Seek(0, SeekOrigin.Begin);
 
-            // Time to cross your fingers.
-            var deser_sv = serialiser.Deserialize(stream) as SemanticVersion;
+        //    // Time to cross your fingers.
+        //    var deser_sv = serialiser.Deserialize(stream) as SemanticVersion;
 
-            Assert.IsNotNull(deser_sv, "Did not deserialise correctly.");
+        //    Assert.IsNotNull(deser_sv, "Did not deserialise correctly.");
 
-            Assert.AreEqual(sv.Major, deser_sv.Major,
-                            "Major versions did not match.");
-            Assert.AreEqual(sv.Minor, deser_sv.Minor,
-                            "Minor versions did not match.");
-            Assert.AreEqual(sv.Patch, deser_sv.Patch,
-                            "Patch versions did not match.");
-            Assert.IsTrue(sv.Identifiers.SequenceEqual(deser_sv.Identifiers),
-                          "Pre-release identifiers did not match.");
-            Assert.IsTrue(sv.Metadata.SequenceEqual(deser_sv.Metadata),
-                          "Build metadata items did not match.");
-        }
+        //    Assert.AreEqual(sv.Major, deser_sv.Major,
+        //                    "Major versions did not match.");
+        //    Assert.AreEqual(sv.Minor, deser_sv.Minor,
+        //                    "Minor versions did not match.");
+        //    Assert.AreEqual(sv.Patch, deser_sv.Patch,
+        //                    "Patch versions did not match.");
+        //    Assert.IsTrue(sv.Identifiers.SequenceEqual(deser_sv.Identifiers),
+        //                  "Pre-release identifiers did not match.");
+        //    Assert.IsTrue(sv.Metadata.SequenceEqual(deser_sv.Metadata),
+        //                  "Build metadata items did not match.");
+        //}
 
         /// <summary>
         /// <para>
@@ -534,9 +534,9 @@ namespace McSherry.SemanticVersioning
         [TestMethod, TestCategory(Category)]
         public void Comparison()
         {
-            Assert.IsTrue(typeof(IComparable<SemanticVersion>)
-                            .IsAssignableFrom(typeof(SemanticVersion)),
-                          "SemanticVersion is not IComparable<SemanticVersion>.");
+            SemanticVersion sv = new SemanticVersion(1, 0);
+            Assert.IsTrue(sv is IComparable<SemanticVersion>,
+                          "SemanticVersion is not IComparable < SemanticVersion >.");
 
             #region Definitions
             var cmp = new SemanticVersion[]
